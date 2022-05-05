382eeda1688ad88a0012533fab589be9dd2099e6
Bodo Schoenenberg <bodo.schoenenberg@web.de>
2022-05-05 21:53:35 +0200

    next build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter  as Router } from "react-router-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
 //stylesheet
 import "./functionBased/App.css"
 ReactDOM.render(
     <React.Fragment>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <TodoContainer />
       </Router>
     </React.Fragment>,
